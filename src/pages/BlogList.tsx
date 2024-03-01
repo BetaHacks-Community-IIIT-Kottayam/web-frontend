4ade4bd047db696e251962488266e08a3c6a66c0
RamKishore <mramki1803@gmail.com>
2024-03-01 12:54:00 +0530

    added search functionality

--- src/pages/BlogList.tsx
+++ src/pages/BlogList.tsx
@@ -5,25 +5,55 @@ import { getAllBlogsService } from "../redux/features/system/contentService";
 import Overlay from "../components/ui/Overlay";
 import ResponsePopup from "../components/ui/ResponsePopup";
 import { retryFetch } from "../redux/features/system/contentSlice";
-const BlogList=()=> {
-  const {blogs,isFetchedAll,status}=useContent();
-  const dispatch=useAppDispatch();
-  const retryHandler=()=>{
-         dispatch(retryFetch());
-  }
+import { useSelector } from "react-redux";
+import { selectSearchQuery } from "../redux/features/blog/searchSlice";
+
+const BlogList = () => {
+  const { blogs, isFetchedAll, status } = useContent();
+  const dispatch = useAppDispatch();
+  const searchQuery = useSelector(selectSearchQuery);
+  const retryHandler = () => {
+    dispatch(retryFetch());
+  };
   useEffect(() => {
-      if(!isFetchedAll){
-         dispatch(getAllBlogsService());
-      }
-  }, [isFetchedAll,status.isError]);
+    if (!isFetchedAll) {
+      dispatch(getAllBlogsService());
+    }
+  }, [isFetchedAll, status.isError]);
 
-  return <div className="flex flex-wrap gap-4 min-h-screen mt-20 mb-4 justify-center items-center w-screen">
-   {status.isLoading && <Overlay message="Fetching blogs, please wait...." />}
-   {status.isError && <ResponsePopup type='error' text={status.errorMessage} onClose={retryHandler} />}
-  {blogs?.map((blog, index) => (
-    <BlogCard key={index} blog={blog} />
-  ))}
-</div>;
-}
+  const blogsStartWithSearchQuery = blogs?.filter((blog: { name: string }) =>
+    blog.name.toLowerCase().startsWith(searchQuery.toLowerCase())
+  );
 
-export default BlogList
+  const blogsIncludingSearchQuery = blogs?.filter(
+    (blog) =>
+      blog.name.toLowerCase().includes(searchQuery.toLowerCase()) &&
+      !blogsStartWithSearchQuery?.includes(blog)
+  );
+
+  let finalFilteredBlogs = (blogsStartWithSearchQuery || []).concat(
+    blogsIncludingSearchQuery || []
+  );
+
+  const blogsToMap = searchQuery !== "" ? finalFilteredBlogs : blogs || [];
+
+  return (
+    <div className="flex flex-wrap gap-4 min-h-screen mt-20 mb-4 justify-center items-center w-screen">
+      {status.isLoading && (
+        <Overlay message="Fetching blogs, please wait...." />
+      )}
+      {status.isError && (
+        <ResponsePopup
+          type="error"
+          text={status.errorMessage}
+          onClose={retryHandler}
+        />
+      )}
+      {blogsToMap?.map((blog, index) => (
+        <BlogCard key={index} blog={blog} />
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
